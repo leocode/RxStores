24b6b1fde5ab51d5b2cd40dc9445836a8123271c
Wojciech Tałałaj <wojtal.pl@gmail.com>
2019-10-22 10:54:19 +0200

    Fixed Store export, probably

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import * as store from './store';
-import { StoreClass, StoreModel } from './store';
+import { Store, StoreClass, StoreModel } from './store';
 import * as helpers from './helpers';
 import { Provider } from './context';
-
-const {
-  Store
-} = store;
 
 export {
   Store,
